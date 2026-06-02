b1c0e299d7c38655e561a3c1465c79d7db3c2e27
piyushkaaher1411-web <piyushkaaher1411@gmail.com>
2026-06-02 11:44:23 +0530

    Update 68- piyushka-aher.py

--- lab/68- piyushka-aher.py
+++ lab/68- piyushka-aher.py
@@ -4,3 +4,27 @@ while i<=10:
   sum= sum+i
   i=i+1
 print(sum)
+#problem 2
+a=4
+b=9
+c=19
+def numbers(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+numbers(a)
+numbers(b)
+numbers(c)
+#problem 3
+a=4
+b=9
+c=19
+def numbers (n):
+    if n%3==0:
+        print("divisible by 3")
+    else:
+        print("not divisible")
+numbers(a)
+numbers(b)
+numbers(c)
