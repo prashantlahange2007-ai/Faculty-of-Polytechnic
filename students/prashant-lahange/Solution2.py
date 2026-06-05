74cacf923028c0024241eb21aa90c5561c2ade80
Nexron <288518361+prashantlahange2007-ai@users.noreply.github.com>
2026-06-05 11:44:27 +0530

    Add vowel counting logic in Solution2.py

---
+++ students/prashant-lahange/Solution2.py
@@ -0,0 +1,16 @@
+s={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s','t','u','v','w','x','y','z'}
+c=0 #c=count
+for x in s:
+  if x=='a':
+    c=c+1
+  elif x=='e':
+    c=c+1
+  elif x=='i':
+    c=c+1
+  elif x=='o':
+    c=c+1
+  elif x=='u':
+    c=c+1
+  else:
+    "continue"
+print (c)
